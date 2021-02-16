96296daccb30615c7f4f43c7ab057f8f68f13b3e
Simona Dimitrova <nimeria1308@gmail.com>
2021-02-17 01:20:01 +0200

    redesign menu CSS

--- menu.php
+++ menu.php
@@ -1,117 +1,95 @@
 <div class="dropdown">
     <div class="dropdown_button">
-        <a class="menu_link">Classes</a>
+        <div class="menu_link">Classes</div>
     </div>
     <div class="dropdown_content">
-        <div class="menu_classes menu_item">
-            <div>
-                <a>
-                    <img alt="class deathknight" src="/resources/images/spell_deathknight_bloodpresence.gif" /> Death Knight
-                </a>
-            </div>
-            <div>
-                <a>
-                    <img alt="class demon hunter" src="/resources/images/ability_demonhunter_spectank.gif" /> Demon Hunter
-                </a>
-            </div>
-            <div>
-                <a>
-                    <img alt="class druid" src="/resources/images/talentspec_druid_feral_bear.gif" /> Druid
-                </a>
-            </div>
-            <div>
-                <a>
-                    <img alt="class hunter" src="/resources/images/ability_marksmanship.gif" /> Hunter
-                </a>
-            </div>
-            <div>
-                <a>
-                    <img alt="class mage" src="/resources/images/spell_fire_firebolt02.gif" /> Mage
-                </a>
-            </div>
-            <div>
-                <a>
-                    <img alt="class monk" src="/resources/images/spell_monk_windwalker_spec.gif" /> Monk
-                </a>
-            </div>
-            <div>
-                <a>
-                    <img alt="class paladin" src="/resources/images/spell_holy_holybolt.gif" /> Paladin
-                </a>
-            </div>
-            <div>
-                <a>
-                    <img alt="class priest" src="/resources/images/spell_shadow_shadowwordpain.gif" /> Priest
-                </a>
-            </div>
-            <div>
-                <a>
-                    <img alt="class rogue" src="/resources/images/ability_stealth.gif" /> Rogue
-                </a>
-            </div>
-            <div>
-                <a>
-                    <img alt="class shaman" src="/resources/images/spell_nature_lightning.gif" /> Shaman
-                </a>
-            </div>
-            <div>
-                <a href="/classes/warlock">
-                    <img alt="class warlock" src="/resources/images/spell_shadow_rainoffire.gif" /> Warlock
-                </a>
-            </div>
-            <div>
-                <a>
-                    <img alt="class warrior" src="/resources/images/ability_warrior_innerrage.gif" /> Warrior
-                </a>
-            </div>
-        </div>
+        <nav class="menu_classes menu_item">
+            <ul>
+                <li class="inactive">
+                    <img alt="Death Knight class icon" src="/resources/images/spell_deathknight_bloodpresence.gif" /> Death Knight
+                </li>
+                <li class="inactive">
+                    <img alt="Demon Hunter class icon" src="/resources/images/ability_demonhunter_spectank.gif" /> Demon Hunter
+                </li>
+                <li class="inactive">
+                    <img alt="Druid class icon" src="/resources/images/talentspec_druid_feral_bear.gif" /> Druid
+                </li>
+                <li class="inactive">
+                    <img alt="Hunter class icon" src="/resources/images/ability_marksmanship.gif" /> Hunter
+                </li>
+                <li class="inactive">
+                    <img alt="Mage class icon" src="/resources/images/spell_fire_firebolt02.gif" /> Mage
+                </li>
+                <li class="inactive">
+                    <img alt="Monk class icon" src="/resources/images/spell_monk_windwalker_spec.gif" /> Monk
+                </li>
+                <li class="inactive">
+                    <img alt="Paladin class icon" src="/resources/images/spell_holy_holybolt.gif" /> Paladin
+                </li>
+                <li class="inactive">
+                    <img width="15" height="15" alt="Priest class icon" src="/resources/images/spell_shadow_shadowwordpain.gif" /> Priest
+                </li>
+                <li class="inactive">
+                    <img width="15" height="15" alt="Rogue class icon" src="/resources/images/ability_stealth.gif" /> Rogue
+                </li>
+                <li class="inactive">
+                    <img width="15" height="15" alt="Shaman class icon" src="/resources/images/spell_nature_lightning.gif" /> Shaman
+                </li>
+                <li class="inactive">
+                    <a href="/classes/warlock" title="Comprehensive guide on Destruction Warlocks">
+                        <img width="15" height="15" alt="Warlock class icon" src="/resources/images/spell_shadow_rainoffire.gif" /> Warlock
+                    </a>
+                </li>
+                <li class="inactive">
+                    <img width="15" height="15" alt="Warrior class icon" src="/resources/images/ability_warrior_innerrage.gif" /> Warrior
+                </li>
+            </ul>
+        </nav>
     </div>
 </div>
 
 <div class="dropdown">
     <div class="dropdown_button">
-        <a class="menu_link">Covenants</a>
+        <div class="menu_link">Covenants</div>
     </div>
     <div class="dropdown_content">
-        <div class="menu_item">
-            <div>
-                <a href="/covenants/kyrian">
-                    <img alt="covenant kyrian" src="/resources/images/ui_sigil_kyrian.gif" /> Kyrian
-                </a>
-            </div>
-            <div>
-                <a>
-                    <img alt="covenant necrolord" src="/resources/images/ui_sigil_necrolord.gif" /> Kyrian
-                </a>
-            </div>
-            <div>
-                <a>
-                    <img alt="covenant night fae" src="/resources/images/ui_sigil_nightfae.gif" /> Night Fae
-                </a>
-            </div>
-            <div>
-                <a>
-                    <img alt="covenant ventyr" src="/resources/images/ui_sigil_venthyr.gif" /> Venthyr
-                </a>
-            </div>
-        </div>
+        <nav class="menu_item">
+            <ul>
+                <li>
+                    <a href="/covenants/kyrian" title="Kyrian: Best abilities for your character">
+                        <img alt="Kyrian covenant icon" src="/resources/images/ui_sigil_kyrian.gif" /> Kyrian
+                    </a>
+                </li>
+                <li class="inactive">
+                    <img alt="Necrolord covenant icon" src="/resources/images/ui_sigil_necrolord.gif" /> Kyrian
+                </li>
+                <li class="inactive">
+                    <img alt="Night covenant icon fae" src="/resources/images/ui_sigil_nightfae.gif" /> Night Fae
+                </li>
+                <li class="inactive">
+                    <img alt="Ventyr covenant icon" src="/resources/images/ui_sigil_venthyr.gif" /> Venthyr
+                </li>
+            </ul>
+        </nav>
     </div>
 </div>
 
 <div class="dropdown">
     <div class="dropdown_button">
-        <a class="menu_link">Dungeons</a>
+        <div class="menu_link">Dungeons</div>
     </div>
     <div class="dropdown_content">
-        <div class="menu_item">
-            <div><a>De Other Side</a></div>
-            <div><a>Halls of Atonement</a></div>
-            <div><a>Mists of Tirna Scithe</a></div>
-            <div><a>The Necrotic Wake</a></div>
-            <div><a>Plaguefall</a></div>
-            <div><a>Sanguine Depths</a></div>
-            <div><a>Spires of Ascention</a></div>
-            <div><a>Theatre of Pain</a></div>
-        </div>
+        <nav class="menu_item">
+            <ul>
+                <li class="inactive">De Other Side</li>
+                <li class="inactive">Halls of Atonement</li>
+                <li class="inactive">Mists of Tirna Scithe</li>
+                <li class="inactive">The Necrotic Wake</li>
+                <li class="inactive">Plaguefall</li>
+                <li class="inactive">Sanguine Depths</li>
+                <li class="inactive">Spires of Ascention</li>
+                <li class="inactive">Theatre of Pain</li>
+            </ul>
+        </nav>
     </div>
 </div>
--- router.php
+++ router.php
@@ -1,5 +1,4 @@
 <?php
-// echo $_SERVER["REQUEST_URI"];
 $path = $_SERVER["REQUEST_URI"];
 
 if ($path == "/") {
